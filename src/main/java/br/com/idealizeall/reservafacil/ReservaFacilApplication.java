49fc91f36303a0c24dc96d0eac6baf1665c19db8
gabrielosorioz <gabrielosorio013@gmail.com>
2024-03-23 11:49:38 -0300

    Initial commit

---
+++ src/main/java/br/com/idealizeall/reservafacil/ReservaFacilApplication.java
@@ -0,0 +1,14 @@
+package br.com.idealizeall.reservafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaFacilApplication.class, args);
+		
+	}
+
+}
