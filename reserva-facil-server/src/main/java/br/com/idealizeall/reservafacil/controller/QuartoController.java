4943ae9b738a7f7ab20562424dc523f9d38a2ce0
gabrielosorioz <gabrielosorio013@gmail.com>
2024-03-25 22:25:56 -0300

    :sparkles: feat(quarto): Introduz a rota de api para salvar um novo quarto no banco de dados.
    - arquivo modificado: `QuartoController.java`

--- reserva-facil-server/src/main/java/br/com/idealizeall/reservafacil/controller/QuartoController.java
+++ reserva-facil-server/src/main/java/br/com/idealizeall/reservafacil/controller/QuartoController.java
@@ -1,4 +1,38 @@
 package br.com.idealizeall.reservafacil.controller;
+import br.com.idealizeall.reservafacil.model.Quarto;
+import br.com.idealizeall.reservafacil.response.QuartoResponse;
+import br.com.idealizeall.reservafacil.service.IQuartoService;
+import br.com.idealizeall.reservafacil.service.QuartoServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/quartos")
 public class QuartoController {
+
+    private final QuartoServiceImpl quartoServiceImpl;
+
+    @PostMapping("/adicionar/quarto")
+    public ResponseEntity<QuartoResponse> adicionarNovoQuarto (
+            @RequestParam("foto") MultipartFile foto,
+            @RequestParam("tipoQuarto")String tipoQuarto,
+            @RequestParam("precoQuarto")BigDecimal precoQuarto) throws SQLException, IOException {
+
+        Quarto quartoSalvo = quartoServiceImpl.adicionarNovoQuarto(foto, tipoQuarto, precoQuarto);
+        QuartoResponse response = new QuartoResponse(quartoSalvo.getId(), quartoSalvo.getTipoQuarto(), quartoSalvo.getPrecoQuarto());
+
+        System.out.println(response);
+        return ResponseEntity.ok(response);
+    }
+
+
+
+
 }
