89b974c6817872f4704b27442c2ed2804ec8402a
gabrielosorioz <gabrielosorio013@gmail.com>
2024-03-25 22:22:52 -0300

    :sparkles: feat(quarto): Introduz os métodos de serviço para manipular os dados do 'Quarto'

--- reserva-facil-server/src/main/java/br/com/idealizeall/reservafacil/service/IQuartoService.java
+++ reserva-facil-server/src/main/java/br/com/idealizeall/reservafacil/service/IQuartoService.java
@@ -1,4 +1,13 @@
 package br.com.idealizeall.reservafacil.service;
 
+import br.com.idealizeall.reservafacil.model.Quarto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
+
 public interface IQuartoService {
+    Quarto adicionarNovoQuarto(MultipartFile foto, String tipoQuarto, BigDecimal precoQuarto) throws IOException, SQLException;
 }
--- reserva-facil-server/src/main/java/br/com/idealizeall/reservafacil/service/QuartoServiceImpl.java
+++ reserva-facil-server/src/main/java/br/com/idealizeall/reservafacil/service/QuartoServiceImpl.java
@@ -1,4 +1,37 @@
 package br.com.idealizeall.reservafacil.service;
 
-public class QuartoServiceImpl {
+import br.com.idealizeall.reservafacil.model.Quarto;
+import br.com.idealizeall.reservafacil.repository.QuartoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.SQLException;
+
+@Service
+@RequiredArgsConstructor
+public class QuartoServiceImpl implements IQuartoService {
+
+    @Autowired
+    private final QuartoRepository quartoRepository;
+
+
+    @Override
+    public Quarto adicionarNovoQuarto(MultipartFile file, String tipoQuarto, BigDecimal precoQuarto) throws IOException, SQLException {
+        Quarto newQuarto = new Quarto();
+        newQuarto.setTipoQuarto(tipoQuarto);
+        newQuarto.setPrecoQuarto(precoQuarto);
+        if(!file.isEmpty()){
+            byte[] fotoBytes = file.getBytes();
+            Blob fotoBlob = new SerialBlob(fotoBytes);
+            newQuarto.setFoto(fotoBlob);
+        }
+
+        return quartoRepository.save(newQuarto);
+
+    }
 }
