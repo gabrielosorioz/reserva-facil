8fe72cdecc987d22853df000ca772aabecc92f04
gabrielosorioz <gabrielosorio013@gmail.com>
2024-03-25 22:21:39 -0300

    :sparkles: feat(quarto): Introduz a classe JPA repository a classe 'Quarto'
    - Esta mudança adiciona uma interface que faz o mapeamento Objeto-Relacional para o banco de dados.

--- reserva-facil-server/src/main/java/br/com/idealizeall/reservafacil/repository/QuartoRepository.java
+++ reserva-facil-server/src/main/java/br/com/idealizeall/reservafacil/repository/QuartoRepository.java
@@ -1,5 +1,11 @@
 package br.com.idealizeall.reservafacil.repository;
 
-public interface QuartoRepository {
+import br.com.idealizeall.reservafacil.model.Quarto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuartoRepository extends JpaRepository<Quarto, Long> {
+
 }
 
