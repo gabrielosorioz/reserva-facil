f6126fb9b0af9827dc61589d4e38b44da7aaeb0d
gabrielosorioz <gabrielosorio013@gmail.com>
2024-03-25 22:17:57 -0300

    :sparkles: feat(quarto): Introduz os campos da entidade 'Quarto' da aplicação

--- reserva-facil-server/src/main/java/br/com/idealizeall/reservafacil/model/Quarto.java
+++ reserva-facil-server/src/main/java/br/com/idealizeall/reservafacil/model/Quarto.java
@@ -1,4 +1,49 @@
 package br.com.idealizeall.reservafacil.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Quarto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String tipoQuarto;
+    private BigDecimal precoQuarto;
+    private boolean isReservado = false;
+    @Lob
+    private Blob foto;
+
+    @OneToMany(mappedBy = "quarto" ,fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<QuartoReservado> reservas;
+
+
+    public Quarto() {
+        this.reservas = new ArrayList<>();
+    }
+
+    public void addReserva(QuartoReservado reserva){
+        if(reservas == null){
+            reservas = new ArrayList<>();
+        }
+        reservas.add(reserva);
+        reserva.setQuarto(this);
+        isReservado = true;
+        String codigoReserva = RandomStringUtils.randomNumeric(10);
+        reserva.setReservaCodigoConfirm(codigoReserva);
+
+    }
 }
